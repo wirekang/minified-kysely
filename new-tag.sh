0ed1a0f31162954da3963f8938eae49e9b8e362a
wirekang <wirekang@gmail.com>
2024-01-16 04:03:00 +0900

    update scripts

--- new-tag.sh
+++ new-tag.sh
@@ -1,16 +1,6 @@
 #/bin/bash
 
 #https://github.com/unegma/bash-functions/blob/main/update.sh
-VERSION=""
-
-#get parameters
-while getopts v: flag; do
-    case "${flag}" in
-    v) VERSION=${OPTARG} ;;
-    esac
-done
-
-#get highest tag number, and add 1.0.0 if doesn't exist
 CURRENT_VERSION=$(git describe --abbrev=0 --tags 2>/dev/null)
 
 if [[ $CURRENT_VERSION == '' ]]; then
@@ -26,18 +16,8 @@ VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == 'major' ]]; then
-    VNUM1=$((VNUM1 + 1))
-elif [[ $VERSION == 'minor' ]]; then
-    VNUM2=$((VNUM2 + 1))
-elif [[ $VERSION == 'patch' ]]; then
-    VNUM3=$((VNUM3 + 1))
-else
-    echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-    exit 1
-fi
+VNUM3=$((VNUM3 + 1))
 
-#create new tag
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
@@ -48,10 +28,8 @@ NEEDS_TAG=$(git describe --contains $GIT_COMMIT 2>/dev/null)
 #only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
     echo "Tagged with $NEW_TAG"
-    git add dist
-    git commit -m "version $NEW_TAG" || echo "no changes"
+    git tag "$NEW_TAG"
     git push --tags
-    git push
 else
     echo "Already a tag on this commit"
 fi
